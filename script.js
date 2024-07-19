d346d41239013edbafabaa02a4f6dfae08c90f88
adarsg <reaemp4@gmail.com>
2024-07-19 10:48:40 +0530

    updated getstarted

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
             var element = document.body;
     element.classList.toggle("dark-mode");
         }
+
+        //togglr b/w imgss
+   const img = document.getElementById('imagesclick');
+   //listner
+   let toggle = true;
+   img.addEventListener('click',function(){
+    toggle = !toggle;
+    if(toggle){
+        img.src ='getstartedimg\\icons8-light-mode-40.png';
+    }else{
+        img.src = 'getstartedimg\\icons8-moon-30.png'
+    }
+    // img.src = 'getstartedimg\\icons8-moon-40.png';
+   })
